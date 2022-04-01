5ffb5b8e422a29ae19eec6efa584b66d6008ac06
imdimasan <info@starlink.by>
2022-04-01 21:07:56 +0300

    INV-02 Start Application
    Configured ESLint, Prettier

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,7 +1,12 @@
-import classes from "./Logo.module.scss"
+import classes from "./Logo.module.scss";
+import React from "react";
 
 const Logo = () => (
-    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png' alt='Logo' className={classes.logo}/>
-)
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png"
+    alt="Logo"
+    className={classes.logo}
+  />
+);
 
-export default Logo
+export default Logo;
