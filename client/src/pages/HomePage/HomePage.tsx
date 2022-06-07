5cd6920006bf4b4d134856ce8c6c032c8382f5a5
imdimasan <info@starlink.by>
2022-06-07 12:00:50 +0300

    INV-15 Get user info
    Added loader. Fixed loading status condition check

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import classes from "./HomePage.module.scss";
 import { FormAuthorization } from "modules";
 import { useSelector } from "react-redux";
-import { userResponseSelector } from "store/global";
+import { loadingStatusSelector } from "store/global";
+import STATUSES from "constants/statuses";
+import { Box, CircularProgress } from "@mui/material";
+import COLORS from "constants/colors";
 
 const HomePage = () => {
-  const user = useSelector(userResponseSelector);
+  const loading = useSelector(loadingStatusSelector);
 
   return (
     <div className={classes.wrapper}>
       <h1>HomePage</h1>
-      {user ? <h2>{user.email}</h2> : <FormAuthorization />}
+      {loading === STATUSES.LOADING ? (
+        <Box sx={{ display: "flex", justifyContent: "center", color: "#000" }}>
+          <CircularProgress sx={{ color: COLORS.LIGHTGRAY }} />
+        </Box>
+      ) : (
+        <FormAuthorization />
+      )}
     </div>
   );
 };
